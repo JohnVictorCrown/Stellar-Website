978e0bd0a293bd8158f8c4d7f84aff2e475ec5bc
John Victor <john.victor.crown@gmail.com>
2026-06-28 12:08:38 -0300

    v1.0.0 release
    - Bump website, Capacitor app, and StellariumCaller to version 1.0.0
    - Beautify CI release page with structured artifact table and install instructions

--- StellariumCaller/app/build.gradle.kts
+++ StellariumCaller/app/build.gradle.kts
@@ -11,8 +11,8 @@ defaultConfig {
         applicationId = "jv.stellariumcaller.stellariumcaller"
         minSdk = 30
         targetSdk = 37
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 1
+        versionName = "1.0.0"
         
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
